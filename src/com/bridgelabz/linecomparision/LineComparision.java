46aa8a0220f1e3459cd0385aadf126b6f0909727
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-08-01 17:05:40 +0530

    Add : Checking equality of two lines

--- src/com/bridgelabz/linecomparision/LineComparision.java
+++ src/com/bridgelabz/linecomparision/LineComparision.java
@@ -10,7 +10,18 @@ public class LineComparision {
 		int y1 = scanner.nextInt();
 		int x2 = scanner.nextInt();
 		int y2 = scanner.nextInt();
+		int a1 = scanner.nextInt();
+		int b1 = scanner.nextInt();
+		int a2 = scanner.nextInt();
+		int b2 = scanner.nextInt();
 		double lengthOfFirstLine = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
 		System.out.println(lengthOfFirstLine);
+		double lengthOfSecondLine = Math.sqrt(Math.pow((a2-a1),2) + Math.pow((b2-b1),2));
+		System.out.println(lengthOfSecondLine);
+		if(lengthOfFirstLine == lengthOfSecondLine) {
+			System.out.println("Two Lines are Equal");
+		}else {
+			System.out.println("Two Lines are not Equal");
+		}
 	}
 }
